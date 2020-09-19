2169c09c6a65a9c1ad6bf0187ec03453f5888aa3
Andre Morpa <andremorpa@gmail.com>
2020-09-19 11:16:30 +0100

    Add highlight alignment

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -3,18 +3,40 @@ import media from 'styled-media-query'
 
 import { HighlightProps } from '.'
 
-type WrapperProps = Pick<HighlightProps, 'backgroundImage'>
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+  `,
+
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     background-image: url(${backgroundImage});
     background-position: center center;
     background-size: cover;
     height: 23rem;
     display: grid;
-    grid-template-areas: 'floatImage content';
-    grid-auto-columns: 1.3fr 2fr;
 
     &::after {
       content: '';
@@ -27,12 +49,14 @@ export const Wrapper = styled.section<WrapperProps>`
     ${media.greaterThan('medium')`
       height:32rem;
     `}
+
+    ${wrapperModifiers[alignment!]()}
   `}
 `
 
 export const FloatImage = styled.img`
   ${({ theme }) => css`
-    grid-area: floatImage;
+    grid-area: floatimage;
     z-index: ${theme.layers.base};
     max-height: 23rem;
     max-width: 100%;
@@ -48,7 +72,6 @@ export const Content = styled.div`
   ${({ theme }) => css`
     grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xxsmall};
 
     ${media.greaterThan('medium')`
@@ -70,7 +93,7 @@ export const Title = styled.h2`
   `}
 `
 
-export const Subtitle = styled.h3`
+export const SubTitle = styled.h3`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.small};
     font-weight: ${theme.font.light};
