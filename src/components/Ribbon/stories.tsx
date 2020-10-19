c52fd7434099277217ee4519620694bc0c5d9d02
Andre Morpa <andremorpa@gmail.com>
2020-10-19 14:04:22 +0100

    Organize storybook Form items

--- src/components/Ribbon/stories.tsx
+++ src/components/Ribbon/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Ribbon, { RibbonProps } from '.'
 
 export default {
-  title: 'Ribbon',
+  title: 'Form/Ribbon',
   component: Ribbon,
   args: {
     children: 'Best Seller'
