107f9dd7cb85ac940add900a7487a8dd93f48365
Andre Morpa <andremorpa@gmail.com>
2020-10-30 13:53:10 +0000

    Add FormSignIn styles

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -5,7 +5,7 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
 
@@ -14,6 +14,8 @@ describe('<FormSignIn />', () => {
     expect(
       screen.getByRole('button', { name: /sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
