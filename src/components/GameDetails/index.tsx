f31e544551aa40dd9c34c5ffe56b54c1bb051431
Andre Morpa <andremorpa@gmail.com>
2020-11-14 09:24:11 +0000

    Add GameDetails to page and fix publisher on GameDetails

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -11,6 +11,7 @@ type Rating = 'pegi0' | 'pegi10' | 'pegi12' | 'pegi14' | 'pegi16' | 'pegi18'
 
 export type GameDetailsProps = {
   developer: string
+  publisher: string
   platforms: Platform[]
   releaseDate: string
   rating: Rating
@@ -22,6 +23,7 @@ const GameDetails = ({
   platforms,
   releaseDate,
   rating,
+  publisher,
   genres
 }: GameDetailsProps) => {
   const platformIcons = {
@@ -66,7 +68,7 @@ const GameDetails = ({
 
         <S.Block>
           <S.Label>Publisher</S.Label>
-          <S.Description>2K</S.Description>
+          <S.Description>{publisher}</S.Description>
         </S.Block>
 
         <S.Block>
--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -6,6 +6,7 @@ import GameDetails, { GameDetailsProps } from '.'
 
 const props: GameDetailsProps = {
   developer: 'Different Tales',
+  publisher: 'Walktrough',
   platforms: ['windows', 'linux', 'mac'],
   releaseDate: '2020-11-21T23:00:00',
   rating: 'pegi0',
@@ -51,6 +52,18 @@ describe('<GameDetails />', () => {
     renderWithTheme(<GameDetails {...props} />)
 
     expect(screen.getByText(/free/i)).toBeInTheDocument()
+  })
+
+  it('should render the developer', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/Different Tales/i)).toBeInTheDocument()
+  })
+
+  it('should render the publisher', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/walktrough/i)).toBeInTheDocument()
   })
 
   it('should render 18+ rating when pegi18', () => {
