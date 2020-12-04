1c55f622ce3c5f2ee09a77f7763ed8ee8c1d114b
Andre Morpa <andremorpa@gmail.com>
2020-12-04 08:31:12 +0000

    Refactor TextField

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -4,7 +4,6 @@ import * as S from './styles'
 export type TextFieldProps = {
   onInput?: (value: string) => void
   label?: string
-  labelFor?: string
   disabled?: boolean
   initialValue?: string
   icon?: React.ReactNode
@@ -18,7 +17,7 @@ const TextField = ({
   iconPosition = 'left',
   label,
   disabled = false,
-  labelFor = '',
+  name,
   initialValue = '',
   error,
   loading,
@@ -36,7 +35,7 @@ const TextField = ({
 
   return (
     <S.Wrapper disabled={disabled} error={!!error} isLoading={!!loading}>
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
@@ -45,6 +44,8 @@ const TextField = ({
           value={value}
           iconPosition={iconPosition}
           disabled={disabled}
+          name={name}
+          {...(label ? { id: name } : {})}
           {...props}
         />
       </S.InputWrapper>
