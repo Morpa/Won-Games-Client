a9b2b6897727a9b659c54570263e20cda8a20c38
Andre Morpa <andremorpa@gmail.com>
2021-01-17 09:41:19 +0000

    Add UserDropdown tests

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<UserDropdown />)
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Morpa" />)
 
-    expect(screen.getByRole('heading', { name: /UserDropdown/i })).toBeInTheDocument()
+    expect(screen.getByText(/morpa/i)).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Morpa" />)
+
+    userEvent.click(screen.getByText(/morpa/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
   })
 })
