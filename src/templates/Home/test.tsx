c6f5d522df52be1f71004543b634a111ac82cb45
Andre Morpa <andremorpa@gmail.com>
2021-01-29 15:32:01 +0000

    Add UpcomingGames and FreeGames, remove unused upcomingMore

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -15,7 +15,6 @@ const props = {
   mostPopularGames: gamesMock,
   upcomingGames: gamesMock,
   upcomingHighlight: highlightMock,
-  upcomingMoreGames: gamesMock,
   freeGames: gamesMock,
   freeHighlight: highlightMock
 }
@@ -44,6 +43,6 @@ describe('<Home />', () => {
 
     expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
 
-    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(4)
   })
 })
