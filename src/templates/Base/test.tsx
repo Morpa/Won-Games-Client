e66fa90a67a3e003da7ad009bd4241bc61d93994
Morpa <andremorpa@gmail.com>
2021-03-12 20:56:03 +0000

    Fix tests after NextAuth

--- src/templates/Base/test.tsx
+++ src/templates/Base/test.tsx
@@ -2,6 +2,12 @@ import { render, screen } from 'utils/test-utils'
 
 import Base from '.'
 
+jest.mock('next-auth/client', () => ({
+  useSession: jest.fn(() => {
+    return [{ session: null }]
+  })
+}))
+
 jest.mock('components/Footer', () => {
   return {
     __esModule: true,
