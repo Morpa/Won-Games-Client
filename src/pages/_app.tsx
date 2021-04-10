e7112931454570a275dc6b9eca0a480726461837
Morpa <andremorpa@gmail.com>
2021-04-10 08:59:44 +0100

    Add initial structure to useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import Head from 'next/head'
 
 import { useApollo } from 'utils/apollo'
 import { CartProvider } from 'hooks/use-cart'
+import { WishlistProvider } from 'hooks/use-wishlist'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
@@ -19,24 +20,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="The best Game Stores in the world!"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world!"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
