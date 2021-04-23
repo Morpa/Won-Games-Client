b42dee9192d4e14f7f681b40c737c79fe2668173
Morpa <andremorpa@gmail.com>
2021-04-23 12:31:55 +0100

    Start Stripe integration adding Elements

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,3 +1,5 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 import { Info } from '@styled-icons/material-outlined/Info'
 
 import Base from 'templates/Base'
@@ -18,6 +20,8 @@ export type CartProps = {
   recommendedHighlight: HighlightProps
 } & CartListProps
 
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
+
 const Cart = ({
   recommendedTitle,
   recommendedGames,
@@ -32,7 +36,10 @@ const Cart = ({
 
         <S.Content>
           <CartList />
-          <PaymentForm />
+
+          <Elements stripe={stripe}>
+            <PaymentForm />
+          </Elements>
         </S.Content>
 
         <S.Text>
