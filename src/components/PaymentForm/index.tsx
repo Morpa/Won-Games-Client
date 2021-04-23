bd339ca445ffcc6d483acfeb193b69d64d09d69e
Morpa <andremorpa@gmail.com>
2021-04-23 17:41:29 +0100

    Integrate the createPaymentIntent to useEffect

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react'
+import { Session } from 'next-auth/client'
 import { CardElement } from '@stripe/react-stripe-js'
 import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ShoppingCart, ErrorOutline } from '@styled-icons/material-outlined'
 
 import { useCart } from 'hooks/use-cart'
+import { createPaymentIntent } from 'utils/stripe/methods'
 import Button from 'components/Button'
 import Heading from 'components/Heading'
 
 import * as S from './styles'
 
-const PaymentForm = () => {
+type PaymentFormProps = {
+  session: Session
+}
+
+const PaymentForm = ({ session }: PaymentFormProps) => {
   const { items } = useCart()
   const [error, setError] = useState<string | null>(null)
   const [disabled, setDisabled] = useState(true)
@@ -17,17 +23,33 @@ const PaymentForm = () => {
   const [freeGames, setFreeGames] = useState(false)
 
   useEffect(() => {
-    if (items.length) {
-      // bater na API /orders/create-payment-intent
-      // enviar os items do carrinho
-      // se eu receber freeGames: true => setFreeGames
-      // faço o fluxo de jogo gratuito
-      // se eu receber um erro
-      // setError
-      // senão o paymentIntent foi válido
-      // setClientSecret
+    async function setPaymentMode() {
+      if (items.length) {
+        // bater na API /orders/create-payment-intent
+        const data = await createPaymentIntent({ items, token: session.jwt })
+
+        // se eu receber freeGames: true => setFreeGames
+        // faço o fluxo de jogo gratuito
+        if (data.freeGames) {
+          setFreeGames(true)
+
+          return
+        }
+
+        // se eu receber um erro
+        // setError
+        if (data.error) {
+          setError(data.error)
+        } else {
+          // senão o paymentIntent foi válido
+          // setClientSecret
+          setClientSecret(data.client_secret)
+        }
+      }
     }
-  }, [items])
+
+    setPaymentMode()
+  }, [items, session])
 
   const handleChange = async (event: StripeCardElementChangeEvent) => {
     setDisabled(event.empty)
