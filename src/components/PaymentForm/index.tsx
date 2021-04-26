abcb4f7949e6e7f5f2e708e78462c06854bc3bf8
Morpa <andremorpa@gmail.com>
2021-04-26 15:22:48 +0100

    Add tests to PaymentForm

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useEffect, useState } from 'react'
+import Link from 'next/link'
 import { Session } from 'next-auth/client'
 import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
@@ -130,9 +131,11 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
           )}
         </S.Body>
         <S.Footer>
-          <Button as="a" fullWidth minimal>
-            Continue shopping
-          </Button>
+          <Link href="/" passHref>
+            <Button as="a" fullWidth minimal>
+              Continue shopping
+            </Button>
+          </Link>
           <Button
             fullWidth
             icon={loading ? <FormLoading /> : <ShoppingCart />}
