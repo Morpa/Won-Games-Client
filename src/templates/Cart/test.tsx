378b852ad962d9c5db4ce5df39446dca4e246096
Morpa <andremorpa@gmail.com>
2021-05-06 09:49:11 +0100

    Fix cart test

--- src/templates/Cart/test.tsx
+++ src/templates/Cart/test.tsx
@@ -7,6 +7,13 @@ import highlightMock from 'components/Highlight/mock'
 import Cart from '.'
 
 const props = {
+  session: {
+    jwt: 'token',
+    user: {
+      email: 'won@games.com'
+    },
+    expires: '13234'
+  },
   recommendedTitle: 'You may like these games',
   recommendedHighlight: highlightMock,
   recommendedGames: gamesMock
@@ -54,13 +61,9 @@ describe('<Cart />', () => {
     expect(
       screen.getByRole('heading', { name: /my cart/i })
     ).toBeInTheDocument()
-
     expect(screen.getByTestId('Mock Cart')).toBeInTheDocument()
-
     expect(screen.getByTestId('Mock PaymentForm')).toBeInTheDocument()
-
     expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
-
     expect(screen.queryByTestId('Mock Empty')).not.toBeInTheDocument()
   })
 })
