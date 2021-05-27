644a426346524a5626686f73d886db66f1718e05
Morpa <andremorpa@gmail.com>
2021-05-27 08:10:51 +0100

    Using click and wait to animation on banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 /// <reference path="../support/index.d.ts" />
 
 describe('Home page', () => {
@@ -7,6 +8,18 @@ describe('Home page', () => {
 
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /cyber shadow/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /the medium/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /Wasteland 3/i })
       cy.findByRole('link', { name: /buy now/i })
     })
   });
